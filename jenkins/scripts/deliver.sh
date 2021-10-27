5cd33d1baf734f1a9341285c9ec4428fda2789ce
mesreekanthh <93214582+mesreekanthh@users.noreply.github.com>
2021-10-27 13:34:39 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:809 to see your Node.js/React application in action.'
+echo 'Visit http://localhost:8090 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 8090:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
